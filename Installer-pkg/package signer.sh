cec6d0aef07f865914e5093acfb776a69ad7c1d8
Allen Hancock <hancock@yesthatallen.com>
2015-11-07 13:39:17 -0800

    sparkle wants an md5

--- Installer-pkg/package signer.sh
+++ Installer-pkg/package signer.sh
@@ -2,6 +2,8 @@
 
 /usr/bin/productsign --sign "Developer ID Installer: Watchman Monitoring, Inc" /Users/Shared/Development/backupminder/Installer-pkg/Build/BackupMinder.pkg /Users/Shared/Development/backupminder/Installer-Signed/BackupMinder.pkg 
 
+# get a signature for sparkle
+md5 /Users/Shared/Development/backupminder/Installer-Signed/BackupMinder.pkg
 
 open /Users/Shared/Development/backupminder/Installer-Signed/
 
